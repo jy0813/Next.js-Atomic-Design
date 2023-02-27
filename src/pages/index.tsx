44d67ee3eb60ef42b57ce9c639ff1600bbf055c9
jy0813 <busbug3@gmail.com>
2023-02-27 15:21:09 +0900

    feat: table Compound Components Setting..

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
-import {Table} from "@/components/organism/TableMain";
+import {Table} from "@/components/molecule/Table";
 
+
+const dataTitle = ['제목', '내용','가격', '체크']
+
+const data = {
+  content:[
+    {
+      id:0,
+      isChecked:true,
+      title:'asdfasfascdwqfc',
+      body:'내용1',
+      price:30000
+    },
+    {
+      id:1,
+      isChecked:true,
+      title:'sadcfzxcvxzvzxv',
+      body:'내용2',
+      price:30000
+    },
+  ]
+}
 
 export default function Home() {
 
@@ -7,16 +28,27 @@ export default function Home() {
       <>
           <Table>
             <Table.Thead>
-              <Table.Tr>
-                <Table.Th/>
-                <Table.Td/>
-              </Table.Tr>
+              <Table.Row>
+                {dataTitle.map((item, index) => <Table.Cell key={index} header>{item}</Table.Cell>)}
+              </Table.Row>
             </Table.Thead>
             <Table.Tbody>
-              <Table.Tr>
-                <Table.Th/>
-                <Table.Td/>
-              </Table.Tr>
+              {data.content.map((item) =>
+                  <Table.Row key={item.id}>
+                    <Table.Cell>
+                      {item.title}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {item.body}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {item.price}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {item.isChecked}
+                    </Table.Cell>
+                  </Table.Row>
+              )}
             </Table.Tbody>
           </Table>
       </>
